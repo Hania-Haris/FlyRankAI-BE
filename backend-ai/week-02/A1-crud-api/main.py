e21d593b4b4230fc60e363925edf7aadf5fa54a3
Hania-Haris <haniaharisanwar@gmail.com>
2026-08-15 23:42:29 +0500

    Stage 3: create task endpoint

--- backend-ai/week-02/A1-crud-api/main.py
+++ backend-ai/week-02/A1-crud-api/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class TaskCreate(BaseModel):
+    title: str
+    description: str = ""
+    completed: bool = False
 
 tasks = [
     {
@@ -36,3 +42,18 @@ def get_task(task_id: int):
             return task
 
     raise HTTPException(status_code=404, detail="Task not found")
+
+@app.post("/tasks", status_code=status.HTTP_201_CREATED)
+def create_task(task: TaskCreate):
+    new_id = max([task["id"] for task in tasks], default=0) + 1
+
+    new_task = {
+        "id": new_id,
+        "title": task.title,
+        "description": task.description,
+        "completed": task.completed
+    }
+
+    tasks.append(new_task)
+
+    return new_task
