ef1ed9b6551e0fb9f2235ea3e73dd710b33bb285
Hania-Haris <haniaharisanwar@gmail.com>
2026-08-15 23:54:13 +0500

    Stage 4: update task endpoints

--- backend-ai/week-02/A1-crud-api/main.py
+++ backend-ai/week-02/A1-crud-api/main.py
@@ -8,6 +8,11 @@ class TaskCreate(BaseModel):
     description: str = ""
     completed: bool = False
 
+class TaskUpdate(BaseModel):
+    title: str
+    description: str = ""
+    completed: bool = False
+
 tasks = [
     {
         "id": 1,
@@ -57,3 +62,14 @@ def create_task(task: TaskCreate):
     tasks.append(new_task)
 
     return new_task
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, task: TaskUpdate):
+    for existing_task in tasks:
+        if existing_task["id"] == task_id:
+            existing_task["title"] = task.title
+            existing_task["description"] = task.description
+            existing_task["completed"] = task.completed
+            return existing_task
+
+    raise HTTPException(status_code=404, detail="Task not found")
